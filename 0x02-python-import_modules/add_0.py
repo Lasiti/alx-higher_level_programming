8f4a1bdffea8cc3bda282775297276e753275521
Festus Lasiti <festuslasiti@gmail.com>
2022-12-01 14:06:50 -0800

    add

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+    a: first interger
+    b: second interger
+
+    returns:
+    the value. a + b
+    """
+    return (a + b)
