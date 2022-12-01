d7a5a229b211c7f6c5b67feb0578e6b1e039954d
Festus Lasiti <festuslasiti@gmail.com>
2022-12-01 15:15:31 -0800

    arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,12 @@
-#r/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
-
-if __name__ == "__main__":
+#!/usr/bin/python3
+if __name__ == '__main__':
     import sys
-    print_arg(sys.argv)
+    i = 1
+    argc = len(sys.argv) - i
+    print('{:d} argument{:s}{:s}'.format(
+        argc, 's' * (argc != 1),
+        ':' if argc > 0 else '.'
+        ))
+    for arg in sys.argv[i:]:
+        print('{:d}: {:s}'.format(i, arg))
+        i += 1
