b432584c3d0be209234134453c4d50993822b245
Festus Lasiti <festuslasiti@gmail.com>
2022-12-05 16:23:35 -0800

    l

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
 def replace_in_list(my_list, idx, element):
-    n = len(my_list)
-    if idx < 0:
-        return my_list
-    elif idx > n - 1:
+    if idx < 0 or idx > len(my_list)-1:
         return my_list
     else:
         my_list[idx] = element
-        return 
+        return my_list
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix != [[]]:
         for row in matrix:
             for elem in row:
                 print("{:d}".format(elem), end=" "
