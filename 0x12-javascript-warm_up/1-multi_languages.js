b981d7c39d6d7884e99f27f25fa87aad0ed777dd
lasiti <festuslasiti@gmail.com>
2023-03-15 16:29:41 -0700

    1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach(
-  elem => console.log(elem)
-);
+const myVar0 = 'C is fun';
+const myVar1 = 'Python is cool';
+const myVar2 = 'Javascript is amazing';
+console.log(myVar0);
+console.log(myVar1);
+console.log(myVar2);
