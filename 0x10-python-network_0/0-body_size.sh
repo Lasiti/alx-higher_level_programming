cffcce9993769fc8e59ed4042016a5b1c7cce870
lasiti <festuslasiti@gmail.com>
2023-03-30 02:21:57 -0700

    0-1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Gets the size of the body of a response from a URL
-curl -sI "$1" | grep -oiE 'Content-Length: [0-9]+' | cut -d ' ' -f2
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
