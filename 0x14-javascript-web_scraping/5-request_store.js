1690e5092f332508f17de44b5aba6b5229919574
Lasiti <111154663+Lasiti@users.noreply.github.com>
2023-07-04 12:14:48 +0300

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/5-request_store.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const request = require('request');
-
-if (process.argv.length > 3) {
-  request
-    .get(`${process.argv[2]}`)
-    .pipe(fs.createWriteStream(process.argv[3]));
-}
